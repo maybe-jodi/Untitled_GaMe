de9d45346775380019ebd43beab4742ea31defc1
NTBeta <78397696+NTBeta@users.noreply.github.com>
2021-05-23 15:33:05 -0500

    Wait function

--- Untitled_GaMe.cs
+++ Untitled_GaMe.cs
@@ -1,3 +1,13 @@
+public void Wait(int time) 
+{           
+    Thread thread = new Thread(delegate()
+    {   
+        System.Threading.Thread.Sleep(time);
+    });
+    thread.Start();
+    while (thread.IsAlive)
+    Application.DoEvents();
+}
 using System;
 using System.Windows.Forms;
 
@@ -6,5 +16,7 @@ class Program
     static void Main()
     {
         MessageBox.Show("Hello, and welcome to Untitled_GaMe!");
+        Wait(5000);
     }
 }
+
