6ad1eebe86214c4311b1ef0a9a3f681129509396
NTBeta <78397696+NTBeta@users.noreply.github.com>
2021-05-26 10:27:50 -0500

    Hmmm

--- Untitled_GaMe.cpp
+++ Untitled_GaMe.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
 int main() {
-sf::RenderWindow window{ { 800, 800 }, "Main Loop - Meeting C++ 2018" }; window.setFramerateLimit(60);
+sf::RenderWindow window{ { 800, 800 }, "Untitled_GaMe" }; window.setFramerateLimit(60);
     while (window.isOpen())
     {
 sf::Event event;
@@ -11,5 +11,16 @@ window.close(); }
 }
         window.clear();
         window.display();
+        sf::Font font;
+if (!font.loadFromFile("DejaVuSans.ttf")) {
+return -1; }
+sf::Text meeting_cpp{ "Hi! Welcome to Untitled_GaMe!", font, 30 };
+meeting_cpp.setPosition({ 0.f, 0.f });
+meeting_cpp.setStyle(sf::Text::Bold);
+meeting_cpp.rotate(0.f);
+window.clear();
+window.draw(Untitled_GaMe);
+window.display();
+  
     }
 }
