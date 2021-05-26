708f6783191e73bc16cbbe70191fec9907a1c281
NTBeta <78397696+NTBeta@users.noreply.github.com>
2021-05-25 19:23:16 -0500

    C# is actually fun not

--- Untitled_GaMe.cs
+++ Untitled_GaMe.cs
@@ -6,14 +6,13 @@ namespace Untitled_GaMe
   {
     static void Main(string[] args)
     {
+      string Input1;
       Console.WriteLine("Hello! Welcome to Untitled_GaMe! I'm Clyde. What's your name?"); 
       
       Console.WriteLine("Enter username:");
+      
+      Input1 = Console.ReadLine();
 
-      //Create a string variable and get user input from the keyboard and store it in the variable
-      string Input1 = Console.ReadLine();
-
-      // Print the value of the variable (userName), which will display the input value
       Console.WriteLine("Oh! Hi {0}",Input1);
     }
   }
