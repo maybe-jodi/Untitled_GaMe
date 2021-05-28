8910bcf692679bc9b8b3b1563630bde40aefdb9c
NTBeta <78397696+NTBeta@users.noreply.github.com>
2021-05-27 21:12:08 -0500

    Yes

--- Untitled_GaMe.cs
+++ Untitled_GaMe.cs
@@ -14,19 +14,9 @@ namespace Untitled_GaMe
     }
   class Program
   {
-    static void Main()
+    public static class Game
     {
-      Console.Title = "Untitled_GaMe - Press Enter";
-      Console.WriteLine("Press Enter");
-
-   ConsoleKeyInfo key = Console.ReadKey();
-
-   if (key.Key.Equals(ConsoleKey.Enter))
-   {
-    Console.WriteLine("Let's start!");
-   }
-      string CharacterName;
-      string Cho1
+      static string CharacterName = "NTBeta";
       Console.Title = "Untitled_GaMe - Start";
       Console.WriteLine("Hello! Welcome to Untitled_GaMe! I'm Clyde."); 
       Console.WriteLine("Now, I need your name. Enter it here: ");
@@ -36,18 +26,7 @@ namespace Untitled_GaMe
       Console.WriteLine("Oh! Hi " + CharacterName + "!\n\n");
       Console.ReadKey();
       Console.WriteLine("The creator of this game, NTBeta, thanks you for downloading the game! Anyways, I hope you enjoy it!");
-      Console.WriteLine("You walk into a bar. There's 2 people there, A bartender and a woman.");
-      Console.WriteLine("You don't know where you are. Do you ask the bartender or the woman where you are?");
-      Console.WriteLine("1 for the bartender, 2 for the woman.");
-      Console.WriteLine("Choose here:")
-      
-      Cho1 = Console.ReadLine();
-      
-      if (Cho1 > 1) 
-      {
-      Console.WriteLine("You're in Malchovitz, off the coast of Galak.");
-      Console.WriteLine("Here, take a map. I bought two earlier. Rather be safe than sorry, eh?");
-      Console.WriteLine("You take the map.");
+    }     
       }
     }
   }
